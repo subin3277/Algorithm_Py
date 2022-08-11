6865cc1bad0ebfe54e5989f924fabdfb46661125
subin3277 <subin3277@naver.com>
2022-08-11 10:14:04 +0900

    Flatten

--- swea/1208.py
+++ swea/1208.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
 T = 10
 for i in range(1,T+1) :
     width = 100
@@ -5,11 +8,28 @@ for i in range(1,T+1) :
     box_list = list(map(int,input().split()))
 
     for j in range(N) :
+        #  최대 최소값 찾기
+        maxV = 1
+        minV = 100
+        for k in box_list:
+            if k >= maxV :
+                maxV = k
+            if k <= minV :
+                minV = k
+        if maxV-minV == 0 or maxV-minV==1:
+            break
         # 가장 높은 곳에서 꺼내기
-        box_list[box_list.index(max(box_list))] -= 1
+        box_list[box_list.index(maxV)] -= 1
         # 가장 낮은 곳에 넣기
-        box_list[box_list.index(min(box_list))] += 1
+        box_list[box_list.index(minV)] += 1
     # 옮긴 후 차이 구하기
-    res = box_list[box_list.index(max(box_list))] - box_list[box_list.index(min(box_list))]
+    maxV = 1
+    minV = 100
+    for k in box_list:
+        if k >= maxV:
+            maxV = k
+        if k <= minV:
+            minV = k
+    res = maxV - minV
     
     print(f'#{i} {res}')
