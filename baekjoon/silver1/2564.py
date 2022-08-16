b001d765d464a4625308ac7ee3a81ea989bd52eb
subin3277 <subin3277@gmail.com>
2022-08-16 17:53:01 +0900

    경비원

--- baekjoon/silver1/2564.py
+++ baekjoon/silver1/2564.py
@@ -8,33 +8,27 @@ mar_list = []
 for i in range(N):
     tmp = list(map(int, input().split()))
     mar_list.append(tmp)
-A, B = map(int, input().split()) # 동근이 위치
+loca = list(map(int, input().split())) # 동근이 위치
+mar_list.append(loca)
 maxres = 0
 res = 0
+for i in range(N+1):
+    if mar_list[i][0] == 2: # 남
+        mar_list[i][1] += H
+    elif mar_list[i][0] == 4: # 동
+        mar_list[i][1] += W
+
 for i in range(N):
-    if mar_list[i][0] == A:
-        res = abs(B - mar_list[i][1])
-    elif mar_list[i][0]+A == 3:
-        res = min(mar_list[i][1]+B, W*2 - mar_list[i][1] - B) + H
-    elif mar_list[i][0]+A == 7:
-        res = min(mar_list[i][1]+W, W*2 - mar_list[i][1] - B) + W
+    if mar_list[i][0] + mar_list[N][0] == 5 or mar_list[i][0] == mar_list[N][0] : # 같거나 마주볼 때
+        maxres = abs(mar_list[i][1] - mar_list[N][1])
+    elif mar_list[i][0] + mar_list[N][0] == 6: # 2, 4번에 있을 때
+        maxres = (W+H)*2 - (mar_list[i][1] + mar_list[N][1])
     else:
-        if abs(A - mar_list[i][0]) == 2:
-            res = B + mar_list[i][1]
-            if A == 2 or A == 4:
-                res += W+H
-        else:
-            if A == 1 or A == 2:
-                res = B - mar_list[i][1]
-            else:
-                res = mar_list[i][1] - B
+        maxres = mar_list[i][1] + mar_list[N][1]
 
-            if A == 1 or A == 4:
-                res = W - res
-            else :
-                res = H + res
-
-    maxres += res
-print(maxres)
-# 해결xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    if maxres >= W+H:
+        maxres = (W + H)*2 - maxres
+    res += maxres
+    print(maxres)
+print(res)
 
