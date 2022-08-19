375f34599246e26c8a09bb7b0eff323ea27b5c10
subin3277 <subin3277@gmail.com>
2022-08-19 17:45:58 +0900

    색종이

---
+++ baekjoon/bronze1/10163.py
@@ -0,0 +1,18 @@
+# 색종이
+import sys
+sys.stdin = open("input.txt", "r")
+
+N = int(input())
+rect_list = [[0]*1001 for _ in range(1001)]
+for k in range(1, N+1):
+    x, y, w, h = map(int, input().split())
+    for i in range(x, x + w):
+        for j in range(y, y + h):
+            rect_list[i][j] = k # 입력된 곳의 위치에 순서번호 입력
+
+
+for j in range(1, N+1):
+    count = 0
+    for i in rect_list:
+        count += i.count(j) # 입력된 갯수 구하기
+    print(count)
