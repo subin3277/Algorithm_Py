6819a4ac0b25463fed2b131ddd135c49fd3d7c51
subin3277 <subin3277@naver.com>
2022-09-10 01:25:16 +0900

    프렌즈4블록

--- programmers/level2/프렌즈4블록.py
+++ programmers/level2/프렌즈4블록.py
@@ -11,26 +11,26 @@ def solution(m, n, board):
         tmp = 0
         for i in range(m-1):
             for j in range(n-1):
-                if new_board[i][j] != 0 and new_board[i][j] == new_board[i][j+1] == new_board[i+1][j] == new_board[i+1][j+1]:
+                if new_board[i][j] != "@" and new_board[i][j] == new_board[i][j+1] == new_board[i+1][j] == new_board[i+1][j+1]:
                     erase.append((i, j))
                     tmp += 4
         if tmp == 0:
             break
 
         for i,j in erase:
-            new_board[i][j] = new_board[i+1][j] = new_board[i][j+1] = new_board[i+1][j+1] = 0
+            new_board[i][j] = new_board[i+1][j] = new_board[i][j+1] = new_board[i+1][j+1] = '@'
 
         for i in range(n):
-            for j in range(m-1, 0, -1):
-                if new_board[i][j] == 0:
-                    new_board[j][i] , new_board[j-1][i] = new_board[j-1][i], new_board[j][i]
-
+            for j in range(m-1, -1, -1):
+                for k in range(j):
+                    if new_board[k+1][i] == "@":
+                        new_board[k][i] , new_board[k+1][i] = new_board[k+1][i], new_board[k][i]
     print(new_board)
     for i in range(m):
-        answer += new_board[i].count(0)
+        answer += new_board[i].count("@")
     return answer
 
-m = 6
-n = 6
-board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
+m = 8
+n = 5
+board = ["HGNHU", "CRSHV", "UKHVL", "MJHQB", "GSHOT", "MQMJJ", "AGJKK", "QULKK"]
 print(solution(m, n, board)) #14
