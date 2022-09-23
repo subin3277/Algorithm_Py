5f54438577a0df183429d7a4065fd448743a83c1
subin3277 <subin3277@gmail.com>
2022-09-23 17:44:42 +0900

    방금 그곡

--- programmers/level2/방금그곡.py
+++ programmers/level2/방금그곡.py
@@ -2,38 +2,55 @@
 
 def solution(m, musicinfos):
     answer = ''
-    music = []
-    codelst = []
+    dic = {'C': 0, 'C#': 1, 'D': 2, 'D#': 3, 'E': 4, 'F': 5, 'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10,'B': 11}
+
+    mcode = ""
+    for j in m:
+        if j == "#":
+            tmp = mcode[-1]
+            mcode = mcode[0:len(mcode) - 1] + str(int(tmp) + 1)
+        else:
+            mcode += str(dic[j])
+    print(mcode)
+    ans = []
+
     for i in range(len(musicinfos)):
         st, et, title, code = map(str, musicinfos[i].split(","))
         if st[0:2] == et[0:2]:
             playtime = int(et[3:5]) - int(st[3:5])
         else:
             playtime = int(st[3:5]) + 60 - int(et[3:5])
-        music.append(title)
+
+        codedig = ""
+        for j in code:
+            if j == "#":
+                tmp = codedig[-1]
+                codedig = codedig[0:len(codedig)-1] + str(int(tmp) + 1)
+            else:
+                codedig += str(dic[j])
         realcode = ""
-        j = k = 0
-        while j < playtime:
-            if k == len(code):
-                k = 0
-            if code[k] == "#":
-                j -= 1
-            realcode += code[k]
-            j += 1
+        k = 0
+        for t in range(playtime):
+            realcode += codedig[k]
             k += 1
-        if j == playtime and k < len(code) and code[k] == "#":
-            realcode += "#"
-        codelst.append(realcode)
-    for i in range(len(codelst)):
-        if m in codelst[i] and (m[-1] == "#" or codelst[i][codelst[i].index(m) + len(m)] != "#"):
-            answer = music[i]
-            break
-    else:
+            if k == len(codedig):
+                k = 0
+        if m in realcode:
+            ans.append([title, playtime])
+    if len(ans) == 0:
         answer = "(None)"
+    elif len(ans) == 1:
+        answer = ans[0][0]
+    else:
+        maxtime = 0
+        for i in ans:
+            if maxtime < i[1]:
+                maxtime = i[1]
+                answer = i[0]
     return answer
 
-m ="ABCDEFG"
-musicinfos = ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+m ="CC#BCC#BCC#"
+musicinfos = ["03:00,03:08,FOO,CC#B"]
 print(solution(m, musicinfos))
 
 # 해결 xxxxxxxxxxxxxxxxxxxxxxxx
