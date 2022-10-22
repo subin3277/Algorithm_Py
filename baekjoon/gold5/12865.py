807b9329328527ab9fef90be3b069957b19c2904
subin3277 <subin3277@naver.com>
2022-10-22 23:07:02 +0900

    정수 삼각형

---
+++ baekjoon/gold5/12865.py
@@ -0,0 +1,15 @@
+# 평법한 배낭
+
+N, K = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * (K+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        if j < lst[i-1][0]:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(lst[i-1][1] + dp[i-1][j-lst[i-1][0]], dp[i-1][j] )
+
+print(dp[N][K])
