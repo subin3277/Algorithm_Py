7d4a50f426b3be8bcdad23ea3f65e34a38e25f39
subin3277 <subin3277@naver.com>
2022-10-30 00:25:14 +0900

    평균은 넘겠지

--- baekjoon/bronze1/4344.py
+++ baekjoon/bronze1/4344.py
@@ -3,3 +3,10 @@
 C = int(input())
 for _ in range(C):
     N, *lst = map(int, input().split())
+    average = sum(lst) / N
+    ans = 0
+    for i in lst:
+        if i > average:
+            ans += 1
+    ans = ans/N * 100
+    print(f"{ans:.3f}%")
