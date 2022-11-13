8beac0694b53efa184cf33c2b84afc8418000101
subin3277 <subin3277@naver.com>
2022-11-13 22:48:22 +0900

    숫자카드

---
+++ baekjoon/silver5/10815.py
@@ -0,0 +1,14 @@
+# 숫자카드
+
+N = int(input())
+N_lst = list(map(int, input().split()))
+M = int(input())
+M_lst = list(map(int, input().split()))
+
+for i in M_lst:
+    if i in N_lst:
+        print(1, end=" ")
+    else:
+        print(0, end=" ")
+
+        # 해결xxxxxxxxxxxxxx
