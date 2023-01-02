bf9e3c7770306a9f0f632ad06c534275c90af384
subin3277 <subin3277@naver.com>
2023-01-03 00:15:48 +0900

    셀프넘버

--- baekjoon/silver5/4673.py
+++ baekjoon/silver5/4673.py
@@ -1,12 +1,24 @@
 # 셀프 넘버
 
+lst = [i for i in range(1, 10001)]
+
 def selfnum(N):
     sum = N
     while N != 0:
         sum += N % 10
         N //= 10
-    return sum
+    if sum < 10001:
+        if sum in lst:
+            lst[lst.index(sum)] = 0
+            selfnum(sum)
+        else:
+            return
+    else :
+        return
 
-for i in range(100):
-    if selfnum(i) != i :
-        print(i)
+for i in lst:
+    if i != 0:
+        selfnum(i)
+for i in lst:
+    if i != 0:
+        print(i) 
