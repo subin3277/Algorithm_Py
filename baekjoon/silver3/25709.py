bba0d7a14a9bfd5dd6ccaebe4393765a175d91a6
subin3277 <subin3277@naver.com>
2023-01-08 17:01:46 +0900

    1 빼기

--- baekjoon/silver3/25709.py
+++ baekjoon/silver3/25709.py
@@ -4,15 +4,24 @@ N = input()
 lst = [(0, N)]
 while lst:
     cnt, num = lst.pop(0)
-    if num == 1:
+    if int(num) == 0:
         print(cnt)
         break
-    lst.append((cnt+1,str(int(num)-1)))
+    lst.append((cnt+1, str(int(num)-1)))
     if "1" in num:
-        if num.index("1") == 0:
-            lst.append((cnt+1, str(num[1:])))
-        elif num.index("1") == len(num)-1 :
-            lst.append((cnt+1, str(num[:-1])))
-        else:
-            A, B = num[num.index("1")+1:],num[:num.index("1")]
-            lst.append((cnt+1, str(int(A)+int(B))))
+        tmp = list(filter(lambda x:num[x] == "1", range(len(num))))
+        for i in tmp:
+            if i == 0:
+                if num[1:]!="":
+                    lst.append((cnt+1, num[1:]))
+                else:
+                    lst.append((cnt+1, "0"))
+            elif i == len(num)-1:
+                if num[:-1]!="":
+                    lst.append((cnt+1, num[:-1]))
+                else:
+                    lst.append((cnt+1, "0"))
+            else:
+                A, B = num[i+1:], num[:i]
+                lst.append((cnt+1, str(int(A)+int(B))))
+# 해결 xxxxxxxxxxxxxxxxxxxx
