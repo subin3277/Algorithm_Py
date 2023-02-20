5a5dc1aaade9d21356db04a547bcd0496d74b795
subinKim <subin3277@gmail.com>
2023-02-20 15:42:02 +0900

    둘만의 암호

--- programmers/level1/둘만의암호.py
+++ programmers/level1/둘만의암호.py
@@ -6,13 +6,20 @@ def solution(s, skip, index):
     skiplist = list(skip)
 
     for i in slst :
-        tmp = ord(i) - 97
+        tmp = ord(i)
         j = 0
+        k = 1
         while (j < index):
             if tmp + 1 < 123 and chr(tmp + 1) not in skiplist :
+                tmp += 1
                 j += 1
-            elif tmp + 1 > 122:
-                return
+            elif tmp + 1 > 122 and chr(tmp + 1 - 26) not in skiplist :
+                tmp -= 25
+                j += 1
+            else :
+                tmp += 1
+        answer += chr(tmp)
+
     return answer
 
 print(solution("aukks", "wbqd", 5))
