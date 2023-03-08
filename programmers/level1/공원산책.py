48405059810c5a6aea747a60f94a1b19bd363500
subinKim <subin3277@naver.com>
2023-03-08 09:01:50 +0900

    공원산책

--- programmers/level1/공원산책.py
+++ programmers/level1/공원산책.py
@@ -3,19 +3,35 @@
 def solution(park, routes):
     answer = []
 
+    W = len(park)
+    H = len(park[0])
+
+    nowx = nowy = 0
+    for i in range(len(park)):
+        if 'S' in park[i]:
+            nowx = i
+            nowy = park[i].find('S')
+
     tmp = [[0] * len(park[0]) for _ in range(len(park))]
     for route in routes:
         loca, num = map(str, route.split())
-        if loca == 'N' :
-            pass
-        elif loca == 'S' :
-            pass
-        elif loca == 'W' :
-            pass
-        else :
-            pass
+        num = int(num)
+        if loca == 'N':
+            if nowx - num >= 0:
+                nowx = nowx - num
+        elif loca == 'S':
+            if nowx + num < W:
+                nowx = nowx + num
+        elif loca == 'W':
+            if nowy - num >= 0:
+                nowy = nowy - num
+        else:
+            if nowy + num < H:
+                nowy = nowy + num
+    answer = [nowx, nowy]
     return answer
 
-park = ["SOO","OOO","OOO"]
-routes = ["E 2","S 2","W 1"]
+
+park = ["SOO", "OOO", "OOO"]
+routes = ["E 2", "S 2", "W 1"]
 print(solution(park, routes))
