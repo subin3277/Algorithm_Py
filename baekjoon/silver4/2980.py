3c8cdbdc8c4827b3c215241ee1074782702f9ab8
subin <subin3277@gmail.com>
2024-01-10 16:18:46 +0900

    도로와 신호등

--- baekjoon/silver4/2980.py
+++ baekjoon/silver4/2980.py
@@ -1,11 +1,23 @@
 # 도로와 신호등
-import sys
-sys.stdin = open("input.txt", "r")
 
 N, L = map(int, input().split())
 light = []
-# True = 빨간불, False = 초록불
 for i in range(N):
-    D, R, G = map(int, input().split())
+    tmp = list(map(int, input().split()))
+    light.append(tmp)
 
-# 해결xxxxxxxxxxxxxxxxxxxxxxxx 다시해보자^^
+light_index = 0
+answer = 0
+nowloaction = 0
+
+while nowloaction < L:
+    if light_index == len(light):
+        answer += (L - nowloaction)
+        break
+    [next_light, R, G] = light[light_index]
+    answer += (next_light - nowloaction)
+    nowloaction += (next_light - nowloaction)
+    if answer % (R + G) < R: # 빨간불. 기다려야함.
+        answer += (R - (answer % (R + G)))
+    light_index += 1
+print(answer)
