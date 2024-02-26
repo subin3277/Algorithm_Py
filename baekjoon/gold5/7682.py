70711f3c25b698ddbd8317073e0e1a5f8c4620ff
subin3277 <subin3277@naver.com>
2024-02-26 22:15:16 +0900

    틱택토

--- baekjoon/gold5/7682.py
+++ baekjoon/gold5/7682.py
@@ -19,6 +19,9 @@ def check(game, X_cnt, O_cnt) :
             elif i[0] == 'X' :
                 X_win = True       
     
+    if X_win and O_win :
+        return "invalid"
+    
     for i in range(3):
         if game[0][i] == game[1][i] == game[2][i]:
             if game[0][i] == 'O' :
@@ -26,19 +29,23 @@ def check(game, X_cnt, O_cnt) :
             elif game[0][i] == 'X':
                 X_win = True
     
+    if X_win and O_win :
+        return "invalid"
+    
     if game[0][0] == game[1][1] == game[2][2] or game[0][2] == game[1][1] == game[2][0] :
         if game[1][1] == 'O' :
             O_win = True
         elif game[1][1] == 'X':
             X_win = True
+
+    if X_win and O_win :
+        return "invalid"
     
     if (X_win and not O_win) or (not X_win and O_win) :
         if (X_win and win == 'O') or (O_win and win == 'X'):
             return "invalid"
         else: 
             return "valid"
-    elif X_win and O_win :
-        return "invalid"
     elif not X_win and not O_win and X_cnt == 5 and O_cnt == 4:
         return "valid"
     
